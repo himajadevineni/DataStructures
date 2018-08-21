89d122119da167a009b850c0f3a8eb9f9c012359
himajadevineni <41759330+himajadevineni@users.noreply.github.com>
2018-08-21 11:33:00 -0400

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,27 +1,25 @@
 import java.util.*;
 
-public class BinarySearch{
+public class BinarySearchExample{
 	public static void main(String[] args){
 		int[] input = {2,5,7,9,11,14,16,17};
-		int key = 9;
+		int key = 15;
 		int index = GetBinarySearch(input,key,0,input.length-1);
-		if(index == -1)
-			System.out.println("Empty Array");
-		System.out.println(key + " is at "+ index +" position");
+		if(index < 0)
+			System.out.println("Not found");
+               else
+		        System.out.println(key + " is at "+ index +" position");
 	}
 	
 	public static int GetBinarySearch(int[] input,int key,int min,int max){
-		if(input.length==0)
-		{
-			return -1;
-		}
-                int l = max-min;
-		int n = input[l/2];
-		if(key==n)
-			return l/2;
-		else if(key < n)
-			return GetBinarySearch(input,key,min,l/2-1);
+                int l = min+(max-min)/2;
+		if(max-min<0)
+		        return -1;         
+		if(key==input[l])
+			return l;
+		else if(key < input[l])
+			return GetBinarySearch(input,key,min,l-1);
 		else
-			return GetBinarySearch(input,key,l/2+1,max); 
+			return GetBinarySearch(input,key,l+1,max); 
 	}
 }
